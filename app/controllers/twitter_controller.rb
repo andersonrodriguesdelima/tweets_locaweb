5c7f34ed434bfa0e7a3ac02ff2dd3275f795e76e
Anderson Rodrigues de Lima <anderson.rodrigues.web@gmail.com>
2017-04-06 13:09:33 -0300

    fazendo mineração dos dados

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,6 +1,8 @@
 class TwitterController < ApplicationController
+	require 'json'
+
 	def most_relevants
-      uri = URI('http://tweeps.locaweb.com.br/tweeps')
+      uri = URI('http://tweeps.locaweb.com.br/tweeps?q=%40locaweb&result_type=mixed')
 
       req = Net::HTTP::Get.new(uri)
       req['Username'] = 'anderson.rodrigues.lima@hotmail.com'
@@ -8,8 +10,12 @@ class TwitterController < ApplicationController
       res = Net::HTTP.start(uri.hostname, uri.port) {|http|
         http.request(req)
       }
+      @result = ['']
+      teste =  JSON.parse res.body
+      teste['statuses'].each do |t|
+      	@result << t['text']
+      end
 
-      render json: res.body
 	  #redirect_to "http://tweeps.locaweb.com.br/tweeps?q=%23locaweb&Username=anderson%2erodrigues%2e%lima%23hotmail%2ecom"
 	end
 end
